9f70f53b90a1b18440a5974ecc1679b4d0b09a8a
ericc70 <eric070@net-c.fr>
2023-01-29 21:09:53 +0100

    add page

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptas
+      ipsum, voluptatibus ea, fuga eveniet beatae quidem rerum quia praesentium
+      tempore? Aspernatur, nisi sunt totam maxime deleniti aut hic architecto.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis
+      corrupti autem impedit atque aliquam ut est alias, deserunt nisi
+      voluptatum cumque velit illum, quia magni, beatae ipsam vel nemo
+      excepturi!
+    </p>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h2>products
+        
+    </h2>
+
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
