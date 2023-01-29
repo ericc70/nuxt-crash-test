f1638abf21313967dab06e2a92dea32aab56fe79
ericc70 <eric070@net-c.fr>
2023-01-29 21:32:16 +0100

    route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <p>Product detail for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum provident ad aliquam consectetur non quas enim sequi soluta at perspiciatis, earum magni voluptate, obcaecati assumenda dolore dicta nulla, corporis aspernatur.</p>
+  </section>
+</template>
+
+<script setup>
+    const {id } = useRoute().params
+</script>
+
+<style>
+
+</style>
